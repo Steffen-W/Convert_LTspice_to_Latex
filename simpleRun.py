73b3bb62d5dda7cb96b40323d8a5b3efe3767eee
Steffen-W <uni@steffenwittemeier.de>
2022-08-09 00:23:44 +0200

    adding an easy to use script

---
+++ simpleRun.py
@@ -0,0 +1,17 @@
+import os
+
+from LTspiceToTexConverter import *
+
+if os.name == 'nt':
+    print("OS: is Windows")
+    path_ltspice = r'C:\Program Files\LTC\LTspiceXVII\lib\sym'
+else:
+    print("OS: is NOT Windows")
+    path_ltspice = '~/.wine/drive_c/Program Files/LTC/LTspiceXVII/lib/sym'
+
+fileName_input = 'Draft.asc'
+print("input:  ", fileName_input)
+
+LtSpiceToLatex(filenameLTspice=fileName_input,
+               lt_spice_directory=os.path.expanduser(path_ltspice),
+               fullExample=1)
